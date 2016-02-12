d514d917a597c1fb55e81655be77e37c5c14a89f
Pyry-Samuli Lahti <pyppe@iki.fi>
2016-02-12 17:01:34 +0200

    Add code for listing/creating events

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 * https://developers.google.com/google-apps/calendar/quickstart/nodejs
 * https://console.developers.google.com/apis/credentials?project=organic-airship-121816
 * https://github.com/google/google-api-nodejs-client/
+* https://developers.google.com/google-apps/calendar/create-events
--- server.js
+++ server.js
@@ -1,8 +1,9 @@
-var google = require('googleapis');
-var OAuth2 = google.auth.OAuth2;
+const google = require('googleapis');
+const OAuth2 = google.auth.OAuth2;
+const calendar = google.calendar('v3');
 const auth = require('./client_secret.json').web;
 
-var oauth2Client = new OAuth2(auth.client_id, auth.client_secret, 'http://localhost:3000/callback');
+var oauth2Client = new OAuth2(auth.client_id, auth.client_secret, 'http://localhost:3000/oauthcallback');
 
 function generateAuthUrl() {
   return oauth2Client.generateAuthUrl({
@@ -11,25 +12,78 @@ function generateAuthUrl() {
   });
 }
 
+function mockEvent() {
+  return {
+    summary: 'Pyppe testaa',
+    location: 'Forever Järvenpää',
+    description: 'This is my test line.\n2nd line.',
+    start: {
+      dateTime: '2016-02-13T09:00:00',
+      timeZone: 'Europe/Helsinki'
+    },
+    end: {
+      dateTime: '2016-02-13T09:30:00',
+      timeZone: 'Europe/Helsinki'
+    },
+    iCalUID: 'pyppetestaa@forever-reservations'
+  };
+}
+
+function fetchEvents(auth, callback) {
+  calendar.events.list({
+    auth: auth,
+    calendarId: 'primary',
+    timeMin: (new Date()).toISOString(),
+    maxResults: 10,
+    singleEvents: true,
+    orderBy: 'startTime'
+  }, callback);
+}
+
 
 
 const express = require('express');
 const app = express();
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.get('/test', function (req, res) {
+app.get('/test', (req, res) => {
   res.redirect(302, generateAuthUrl());
 });
 
-app.get('/callback', function (req, res) {
-  console.log(req);
-  res.send(JSON.strigify(req));
+app.get('/oauthcallback', (req, res) => {
+  oauth2Client.getToken(req.query.code, (err, tokens) => {
+    if (!err) {
+      oauth2Client.setCredentials(tokens); // TODO: Cannot be global!
+      /*
+      fetchEvents(oauth2Client, (err, response) => {
+        if (err) {
+          console.log('The API returned an error: ' + err);
+          return;
+        }
+        var events = response.items;
+        res.send(events);
+      });
+      */
+      calendar.events.import({
+        auth: oauth2Client,
+        calendarId: 'primary',
+        resource: mockEvent()
+      }, (err, response) => {
+        if (err) {
+          console.error('Error: %o', err);
+          res.status(500).send(err);
+          return;
+        }
+        res.send(response);
+      });
+    }
+  });
 });
 
 
-app.listen(3000, function () {
+app.listen(3000, () => {
   console.log('Example app listening on port 3000!');
 });
 
