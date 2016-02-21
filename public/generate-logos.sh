ca1f2b009c3bcd38dcd0578951da4f2b189831d4
Pyry-Samuli Lahti <pyppe@iki.fi>
2016-02-21 13:59:33 +0200

    Fine-tune jumbotron styles

--- public/generate-logos.sh
+++ public/generate-logos.sh
@@ -3,4 +3,5 @@
 readonly PROGDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 convert -trim -bordercolor '#e2001a' -border 200x20 $PROGDIR/img/forever-gym-reservations.png $PROGDIR/img/logo-wide.png
+convert -trim $PROGDIR/img/forever-gym-reservations.png $PROGDIR/img/logo.png
 
